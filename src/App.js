a8753979d2f8d6fd21aca3c0605a456c516d7279
Niraj Paghadar <niraj.paghadar@prowerse.com>
2021-05-31 18:55:35 +0530

    Implement Hamburger Menu in To-Do Application

--- src/App.js
+++ src/App.js
@@ -3,23 +3,40 @@ import Form from './components/Form';
 import Header from './components/Header';
 import React from 'react'
 import TodoList from './components/TodoList';
+import { Route, Switch } from "react-router-dom"
+import About from './components/About';
+import NotMatch from './components/NotMatch';
+import Navbar from './components/Navbar';
 
 const App = () => {
 
   return (
-    <div className="container">
-      <div className="app-wrapper">
-        <div>
-          <Header />
+    <>
+      <Navbar />
+      <Switch>
+      <Route exact path="/">
+        <div className="container">
+          <div className="app-wrapper">
+            <div>
+              <Header />
+            </div>
+            <div>
+              <Form />
+            </div>
+            <div>
+              <TodoList />
+            </div>
+          </div>
         </div>
-        <div>
-          <Form />
-        </div>
-        <div>
-          <TodoList />
-        </div>
-      </div>
-    </div>
+      </Route>
+      <Route path="/about" component={About}>
+        <About />
+      </Route>
+      <Route path="*">
+        <NotMatch />
+      </Route>
+      </Switch>
+    </>
   );
 }
 
